411e8cd861793955aae9d48a8da6cc54a153ce52
Adrián Núñez <anunezpedroche@gmail.com>
2020-01-13 20:22:31 +0100

    Prueba
    Prueba

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
+
 let port = process.env.PORT;
 if(port==null|| port==""){
     port = 3000;
@@ -14,13 +15,14 @@ const mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({
     extended:true
 }));
+
 app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(dbConfig.url,{
     useNewUrlParser:true}).then(()=>{
-        console.log(" * Cargada y preparada en 2019");
+        console.log(" * Cargada y preparada en 2020");
 
     }).catch(err=>{
         console.log("Algo ha pasado...saliendo : ",err);
@@ -28,14 +30,7 @@ mongoose.connect(dbConfig.url,{
     });
 
 
-/*const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://adminFallero:mongo1234@cluster0-yzrtq.mongodb.net/test";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("mongofallero").collection("mongofallero");
-  // perform actions on the collection object
-  client.close();
-});*/
+
 // Paginas publicas (estaticas)
 
 app.use(express.static(path.join(__dirname, 'MongoFallero/public')));
